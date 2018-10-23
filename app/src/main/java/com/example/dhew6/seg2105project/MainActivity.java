b4e255ffbea2aff8665174720034a86aff960a04
DavidHewWing <dhew67@gmail.com>
2018-10-23 12:29:41 -0400

    testing gitignore

--- app/src/main/java/com/example/dhew6/seg2105project/MainActivity.java
+++ app/src/main/java/com/example/dhew6/seg2105project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dhew6.seg2105project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //testing gitignore
+        TextView test = findViewById(R.id.testText);
+        test.setText("Testing GitIgnore");
+
     }
 }
