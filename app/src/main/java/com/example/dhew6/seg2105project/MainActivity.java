a9c4e9ba3ccaf1922f6afa203db7fc71b3e888c5
Keith Tang <ktang081@uottawa.ca>
2018-10-24 17:23:24 -0400

    Finished welcome screen, accessible through Create and Login Screens

--- app/src/main/java/com/example/dhew6/seg2105project/MainActivity.java
+++ app/src/main/java/com/example/dhew6/seg2105project/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.dhew6.seg2105project;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         //removes the blank fragment from backstack
         //so when you click back you close the application.
         manager.popBackStack();
-
 
     }
 }
