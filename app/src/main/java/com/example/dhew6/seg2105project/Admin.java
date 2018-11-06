0edd23a8b25a83b6ef3195db59b3591c4a6bf8bb
DavidHewWing <dhew67@gmail.com>
2018-11-06 00:00:48 -0500

    working on adding functionality

--- app/src/main/java/com/example/dhew6/seg2105project/Admin.java
+++ app/src/main/java/com/example/dhew6/seg2105project/Admin.java
@@ -1,9 +1,35 @@
 package com.example.dhew6.seg2105project;
 
+import java.util.HashMap;
+
 public class Admin extends User {
 
-    public Admin(String name, String username, String password, String email) {
+    private HashMap<String, Service> services;
+    private static Admin instance = null;
+
+    private Admin(String name, String username, String password, String email) {
         super(name, username, password, email);
+        this.services = new HashMap<>();
+
+    }
+
+    public static Admin getInstance() {
+        if (instance == null) {
+            instance = new Admin("admin", "admin", "admin", "admin");
+        }
+        return instance;
+    }
+
+    public HashMap<String, Service> getServices() {
+        return services;
+    }
+
+    public void setServices(HashMap<String, Service> services) {
+        this.services = services;
+    }
+
+    public void updateService(String key, Service service){
+        services.put(key,service);
     }
 
     @Override
--- app/src/main/java/com/example/dhew6/seg2105project/WelcomeScreen.java
+++ app/src/main/java/com/example/dhew6/seg2105project/WelcomeScreen.java
@@ -2,6 +2,8 @@ package com.example.dhew6.seg2105project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,7 +11,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +21,15 @@ import java.util.ArrayList;
 
 public class WelcomeScreen extends AppCompatActivity {
 
-    //init commit of branch
-
     DatabaseHelper myDB;
-    TextView roleTextView, usernameTextView;
+    TextView roleTextView, usernameTextView, titleListView;
     ArrayList<User> users;
+    ArrayList<String> usernames;
     ListView listview;
+    Switch listviewSwitch;
+    Button addServiceButton;
+    ConstraintLayout layout;
+    boolean userType;
 
     /**
      * inflates the menu for a log out option
@@ -65,20 +72,44 @@ public class WelcomeScreen extends AppCompatActivity {
         boolean isAdmin = username.equals("admin");
         roleTextView = findViewById(R.id.roleTextView);
         usernameTextView = findViewById(R.id.usernameTextView);
+        listviewSwitch = findViewById(R.id.listviewSwitch);
+        addServiceButton = findViewById(R.id.addServiceButton);
+        layout = findViewById(R.id.layout);
+        titleListView = findViewById(R.id.listviewTitle);
+
+        listviewSwitch.setVisibility(View.INVISIBLE);
+        addServiceButton.setVisibility(View.INVISIBLE);
+
+        //true = users. false = services
+        userType = true;
+
+        ConstraintSet set = new ConstraintSet();
+        set.clone(layout);
+        set.connect(R.id.listview, ConstraintSet.BOTTOM, R.id.layout, ConstraintSet.BOTTOM, 24);
+        set.applyTo(layout);
+
+        switchClicked();
 
         //if the user is admin then display all users in a listview
         if (isAdmin) {
             username = "Admin";
             role = "Admin";
-            User admin = new Admin("admin", "admin", "admin", "admin");
+            User admin =   Admin.getInstance();
+
             listview = findViewById(R.id.listview);
-            ArrayList<String> usernames = new ArrayList<>();
+            listviewSwitch.setVisibility(View.VISIBLE);
+
+            //populate listview with usernames
+            usernames = new ArrayList<>();
             for (int i = 0; i < users.size(); i++) {
                 usernames.add(users.get(i).getUsername());
             }
             ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usernames);
             listview.setAdapter(adapter);
             adapter.notifyDataSetChanged();
+
+            //populates listview with services
+
         } else if (!isAdmin) {
             //if they aren't an admin then get the user then display the type.
             User user = myDB.getUser(username);
@@ -93,5 +124,25 @@ public class WelcomeScreen extends AppCompatActivity {
 
     }
 
+    public void switchClicked(){
+
+        listviewSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //now in the services listview
+                    listviewSwitch.setText("Services");
+                    titleListView.setText("Services Available");
+                }else{
+                    //now in users listview
+                    listviewSwitch.setText("Users");
+                    titleListView.setText("Users in the Database");
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(WelcomeScreen.this, android.R.layout.simple_list_item_1, usernames);
+                    listview.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
 
 }
